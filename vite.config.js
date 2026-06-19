bb0cdae03a2632388e5a6db7979fec7a7bd49760
Dream Helpers <ops@dreamhelpers.local>
2026-06-18 18:29:46 -0700

    Ignore build artifacts

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-var _a;
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-// For GitHub Pages, set base to '/<repo-name>/'. Override with VITE_BASE if needed.
-export default defineConfig({
-    plugins: [react()],
-    base: (_a = process.env.VITE_BASE) !== null && _a !== void 0 ? _a : '/dreamhelpers-ops/',
-});
